a21478806bffc99af18fb911edb62ace1a8b77e4
hzuellig <zuellig@internauta.ch>
2024-06-09 11:52:11 +0200

    Add files via upload

---
+++ 00_basic_example/sketch.js
@@ -0,0 +1,27 @@
+//1. Load Image
+
+// 8. load another image 
+
+// 9. use microfon input to change the copied part
+
+function setup() {
+  createCanvas(400, 400);
+
+  //6. create a Slider and a Variable to store the value
+}
+
+function draw() {
+  background(220);
+
+  // 2. Display the image 
+
+  // 3. Copy parts  of the image to the canvas -- use function get() and mouseX, mouseY
+
+  // 4. add some randomness
+
+
+  // 7. Use the slider value to change the size of the copied part
+}
+
+
+// 5. add a save function
